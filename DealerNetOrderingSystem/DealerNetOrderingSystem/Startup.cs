7e28e876ddea1d47d1dad0e6ca743432920ca0ce
NeilOCV <31700601+NeilOCV@users.noreply.github.com>
2017-10-11 10:34:10 +0200

    First check-in

---
+++ DealerNetOrderingSystem/DealerNetOrderingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DealerNetOrderingSystem.Startup))]
+namespace DealerNetOrderingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
