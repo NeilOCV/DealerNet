5fc2ae21dbf0b87668dac5ab3e2745ab23254482
NeilOCV <31700601+NeilOCV@users.noreply.github.com>
2017-10-11 10:58:01 +0200

    Tried something.
    Not what I wanna do.  Revert this!

---
+++ DealerNetOrderingSystemAPI/DAL/Partials/categories.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public partial class categories
+    {
+        #region Variables
+
+        #region Public
+
+
+
+        #endregion
+
+        #region Private
+
+
+
+        #endregion
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Methods
+
+        #region Public
+
+        #region CRUD
+
+        #region Create (POST)
+
+        public int Create(categories obj)
+        {
+            return 0;
+        }
+
+        #endregion
+
+        #region Read (GET)
+
+
+
+        #endregion
+
+        #region Update (PUT)
+
+
+
+        #endregion
+
+        #region Delete (DELETE)
+
+
+
+        #endregion
+
+        #endregion
+
+        #endregion
+
+        #region Private
+
+
+
+        #endregion
+
+        #endregion
+    }
+}
