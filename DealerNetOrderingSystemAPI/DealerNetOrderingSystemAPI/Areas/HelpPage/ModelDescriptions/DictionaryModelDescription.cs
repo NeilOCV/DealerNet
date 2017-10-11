a43b8d3896902dbe7e15c58d8657488071caebb5
NeilOCV <31700601+NeilOCV@users.noreply.github.com>
2017-10-11 10:58:58 +0200

    Revert "First check-in"
    This reverts commit 7e28e876ddea1d47d1dad0e6ca743432920ca0ce.

--- DealerNetOrderingSystemAPI/DealerNetOrderingSystemAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DealerNetOrderingSystemAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- DealerNetOrderingSystemAPI/DealerNetOrderingSystemAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DealerNetOrderingSystemAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
